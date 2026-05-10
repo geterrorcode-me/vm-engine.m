dd4e712279d97c270f4199246a6b2f3d7824883f
getError <geterrorcode@gmail.com>
2026-05-11 03:22:36 +0700

    Modular

---
+++ jni/surface_vm.cpp
@@ -0,0 +1,8 @@
+#include "vm_internal.h"
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+void init_virtual_display() {
+    LOGI("vMeer Surface Virtualization: Initializing Virtual Display...");
+    // Di sini nanti tempat logic Gralloc Proxy & SurfaceFlinger redirection
+}
--- vmeer-module/src/main/jni/Android.mk
+++ vmeer-module/src/main/jni/Android.mk
@@ -1,10 +1,30 @@
 LOCAL_PATH := $(call my-dir)
+ROOT_JNI := $(LOCAL_PATH)/../../../../jni
+
+# --- 1. Core Engine Bootstrap ---
 include $(CLEAR_VARS)
 LOCAL_MODULE    := vmeer_engine
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../include
-LOCAL_SRC_FILES := \
-    ../../../../jni/vm_engine.cpp \
-    ../../../../jni/binder_vm.cpp \
-    ../../../../jni/vfs_hook.cpp
+LOCAL_SRC_FILES := $(ROOT_JNI)/vm_engine.cpp
 LOCAL_LDLIBS    := -llog -landroid
+include $(BUILD_SHARED_LIBRARY)
+
+# --- 2. Binder Namespace & Proxy ---
+include $(CLEAR_VARS)
+LOCAL_MODULE    := binder_vm
+LOCAL_SRC_FILES := $(ROOT_JNI)/binder_vm.cpp
+LOCAL_LDLIBS    := -llog
+include $(BUILD_SHARED_LIBRARY)
+
+# --- 3. VFS Bridge & ROM Mounting ---
+include $(CLEAR_VARS)
+LOCAL_MODULE    := vfs_vm
+LOCAL_SRC_FILES := $(ROOT_JNI)/vfs_hook.cpp
+LOCAL_LDLIBS    := -llog
+include $(BUILD_SHARED_LIBRARY)
+
+# --- 4. Surface & Display (Placeholder untuk Prioritas 1) ---
+include $(CLEAR_VARS)
+LOCAL_MODULE    := surface_vm
+LOCAL_SRC_FILES := $(ROOT_JNI)/surface_vm.cpp
+LOCAL_LDLIBS    := -llog -lEGL -lGLESv2
 include $(BUILD_SHARED_LIBRARY)
