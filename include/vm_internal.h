4efa5b9bfb28142a7f1e6a4f49c0140d43184907
getError <geterrorcode@gmail.com>
2026-05-11 04:03:15 +0700

    Perbaikan Android.mk (Modular & Fixed Path)

--- include/vm_internal.h
+++ include/vm_internal.h
@@ -1,9 +1,16 @@
 #ifndef VM_INTERNAL_H
 #define VM_INTERNAL_H
+
 #include <jni.h>
 #include <android/log.h>
+
 #define LOG_TAG "vMeer_Engine"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-int init_vfs_bridge(const char* path);
-void start_virtual_binder();
-#endif
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+// Deklarasi fungsi antar-modul
+void init_egl_bridge();
+void setup_virtual_buffer_queue();
+void init_virtual_display();
+
+#endif // VM_INTERNAL_H
--- vmeer-module/src/main/jni/Android.mk
+++ vmeer-module/src/main/jni/Android.mk
@@ -1,10 +1,20 @@
 LOCAL_PATH := $(call my-dir)
+# Jalur ke folder jni utama (4 tingkat ke atas dari vmeer-module/src/main/jni)
 ROOT_JNI := $(LOCAL_PATH)/../../../../jni
+# Jalur ke folder include utama
+ROOT_INCLUDE := $(LOCAL_PATH)/../../../../include
+
+# --- COMMON SETTINGS ---
+define vmeer-module-defaults
+    LOCAL_C_INCLUDES := $(ROOT_INCLUDE)
+    LOCAL_LDLIBS := -llog -landroid
+endef
 
 # --- 1. EGL Bridge ---
 include $(CLEAR_VARS)
 LOCAL_MODULE    := egl_bridge
 LOCAL_SRC_FILES := $(ROOT_JNI)/egl_bridge.cpp
+LOCAL_C_INCLUDES := $(ROOT_INCLUDE)
 LOCAL_LDLIBS    := -llog -lEGL
 include $(BUILD_SHARED_LIBRARY)
 
@@ -12,20 +22,22 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := bufferqueue_vm
 LOCAL_SRC_FILES := $(ROOT_JNI)/bufferqueue_vm.cpp
-# Hapus -lui -lgui dari sini karena mereka private API
-LOCAL_LDLIBS    := -llog -landroid
+LOCAL_C_INCLUDES := $(ROOT_INCLUDE)
+LOCAL_LDLIBS    := -llog
 include $(BUILD_SHARED_LIBRARY)
 
 # --- 3. Surface VM ---
 include $(CLEAR_VARS)
 LOCAL_MODULE    := surface_vm
 LOCAL_SRC_FILES := $(ROOT_JNI)/surface_vm.cpp
-LOCAL_LDLIBS    := -llog -lEGL -lGLESv2 -landroid
+LOCAL_C_INCLUDES := $(ROOT_INCLUDE)
+LOCAL_LDLIBS    := -llog -lEGL -lGLESv2
 include $(BUILD_SHARED_LIBRARY)
 
 # --- 4. Core vMeer Engine ---
 include $(CLEAR_VARS)
 LOCAL_MODULE    := vmeer_engine
 LOCAL_SRC_FILES := $(ROOT_JNI)/vm_engine.cpp
-LOCAL_LDLIBS    := -llog -landroid
+LOCAL_C_INCLUDES := $(ROOT_INCLUDE)
+LOCAL_LDLIBS    := -llog
 include $(BUILD_SHARED_LIBRARY)
