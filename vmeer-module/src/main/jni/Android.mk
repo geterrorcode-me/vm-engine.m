46764c33c2f0b264a96a859a5fbc029469eaf0c1
getError <geterrorcode@gmail.com>
2026-05-11 19:24:28 +0700

    Fix: Fully modular CMake with static helper and forward declarations

--- vmeer-module/src/main/jni/Android.mk
+++ vmeer-module/src/main/jni/Android.mk
@@ -1,22 +1,36 @@
 LOCAL_PATH := $(call my-dir)
-ROOT_JNI := $(LOCAL_PATH)/../../../../jni
-ROOT_INCLUDE := $(LOCAL_PATH)/../../../../include
 
-# --- ShadowHook Prebuilt (Asumsi Anda sudah mendownload library-nya) ---
+# Jalur relatif yang disesuaikan dengan struktur folder Anda
+# Pastikan jalur ini benar-benar mengarah ke folder jni utama
+ROOT_JNI := $(LOCAL_PATH)
+
+# --- ShadowHook Prebuilt ---
 include $(CLEAR_VARS)
 LOCAL_MODULE := shadowhook
+# Sesuaikan jalur libshadowhook.so sesuai lokasi aslinya
 LOCAL_SRC_FILES := $(ROOT_JNI)/3rdparty/shadowhook/libs/$(TARGET_ARCH_ABI)/libshadowhook.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 # --- vMeer Engine Core ---
 include $(CLEAR_VARS)
 LOCAL_MODULE    := vmeer_engine
-LOCAL_C_INCLUDES := $(ROOT_INCLUDE) $(ROOT_JNI)
-LOCAL_SRC_FILES := \
-    $(ROOT_JNI)/vm_engine.cpp \
-    $(ROOT_JNI)/binder/binder_rewriter.cpp \
-    $(ROOT_JNI)/vfs_hook.cpp
 
+# Tambahkan jalur folder 'include' agar vm_internal.h ditemukan
+LOCAL_C_INCLUDES := \
+    $(ROOT_JNI) \
+    $(ROOT_JNI)/include \
+    $(ROOT_JNI)/bridge \
+    $(ROOT_JNI)/binder \
+    $(ROOT_JNI)/3rdparty/shadowhook/include
+
+LOCAL_SRC_FILES := \
+    vmeer_engine.cpp \
+    vfs_hook.cpp \
+    bridge/hook_bridge.cpp \
+    binder/binder_rewriter.cpp
+
+# Library yang dibutuhkan untuk linking
 LOCAL_SHARED_LIBRARIES := shadowhook
-LOCAL_LDLIBS    := -llog -landroid -ldl
+LOCAL_LDLIBS    := -llog -landroid -lEGL -ldl
+
 include $(BUILD_SHARED_LIBRARY)
