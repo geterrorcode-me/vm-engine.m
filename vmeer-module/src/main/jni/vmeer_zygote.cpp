aba4f7aaa03547d4afdb5ef60a3b178e7d78e2af
getError <geterrorcode@gmail.com>
2026-05-13 00:40:59 +0700

    jint_array jintArray Array untuk tipe data int. jstring_array jobjectArray Array of String di JNI diproses sebagai jobjectArray.

--- vmeer-module/src/main/jni/vmeer_zygote.cpp
+++ vmeer-module/src/main/jni/vmeer_zygote.cpp
@@ -1,9 +1,9 @@
 #include <jni.h>
 #include <android/log.h>
 #include <unistd.h>
+#include <string>
 #include "shadowhook.h"
 #include "include/vmeer_zygote.h"
-#include "include/vmeer_context.h"
 
 #define LOG_TAG "vMeer_Zygote"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -11,38 +11,30 @@
 namespace vmeer {
 namespace zygote {
 
-    // Pointer ke fungsi asli di libandroid_runtime.so
+    // Perbaikan tipe data JNI (jintArray & jobjectArray)
     typedef jint (*nativeForkAndSpecialize_t)(
-        JNIEnv*, jclass, jint, jint, jint_array, jint, jobject, jint, 
-        jstring, jstring, jint_array, jint_array, jboolean, jstring, 
-        jstring, jboolean, jstring_array, jstring_array, jboolean, jboolean);
+        JNIEnv*, jclass, jint, jint, jintArray, jint, jobject, jint, 
+        jstring, jstring, jintArray, jintArray, jboolean, jstring, 
+        jstring, jboolean, jobjectArray, jobjectArray, jboolean, jboolean);
 
     static nativeForkAndSpecialize_t orig_nativeForkAndSpecialize = nullptr;
 
-    /**
-     * PROXY: Fungsi ini yang akan dieksekusi setiap kali Android menjalankan aplikasi baru.
-     */
     jint proxy_nativeForkAndSpecialize(
-        JNIEnv* env, jclass clazz, jint uid, jint gid, jint_array gids, jint runtime_flags,
+        JNIEnv* env, jclass clazz, jint uid, jint gid, jintArray gids, jint runtime_flags,
         jobject rlimits, jint mount_external, jstring se_info, jstring nice_name,
-        jint_array fds_to_close, jint_array fds_to_ignore, jboolean is_child_zygote,
+        jintArray fds_to_close, jintArray fds_to_ignore, jboolean is_child_zygote,
         jstring instruction_set, jstring app_data_dir, jboolean is_top_app,
-        jstring_array pkg_data_info_list, jstring_array whitelisted_data_info_list,
+        jobjectArray pkg_data_info_list, jobjectArray whitelisted_data_info_list,
         jboolean bind_mount_app_data_fp, jboolean bind_mount_app_storage_fp) {
 
-        // 1. Identifikasi proses: Apakah ini paket yang harus masuk vMeer?
         const char* c_nice_name = env->GetStringUTFChars(nice_name, nullptr);
         
         if (c_nice_name && std::string(c_nice_name).find("com.vmeer.virtual") != std::string::npos) {
             LOGI("Zygote: Hooked birth of virtual process -> %s", c_nice_name);
-            
-            // 2. Manipulasi UID/GID untuk isolasi (Evolusi Virtual UID)
-            // uid = 20000 + (uid % 10000); // Contoh offset UID virtual
         }
 
         if (c_nice_name) env->ReleaseStringUTFChars(nice_name, c_nice_name);
 
-        // 3. Panggil fungsi asli agar proses benar-benar tercipta
         return orig_nativeForkAndSpecialize(
             env, clazz, uid, gid, gids, runtime_flags, rlimits, mount_external, se_info, 
             nice_name, fds_to_close, fds_to_ignore, is_child_zygote, instruction_set, 
@@ -51,7 +43,6 @@ namespace zygote {
     }
 
     void HookForkAndSpecialize() {
-        // Mendekati Zygote64 pada libandroid_runtime.so
         void* stub = shadowhook_hook_sym_name(
             "libandroid_runtime.so",
             "com_android_internal_os_Zygote_nativeForkAndSpecialize",
@@ -60,7 +51,7 @@ namespace zygote {
         );
 
         if (stub) {
-            LOGI("Zygote Engine: nativeForkAndSpecialize successfully redirected.");
+            LOGI("Zygote Engine: Hook point fixed and redirected.");
         }
     }
 
