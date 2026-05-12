ac0ea3511369c46ac5aa51f32586b32ebfa393fa
getError <geterrorcode@gmail.com>
2026-05-13 02:04:24 +0700

    implementation

--- vmeer-module/src/main/jni/vmeer_pms.cpp
+++ vmeer-module/src/main/jni/vmeer_pms.cpp
@@ -7,40 +7,85 @@
 namespace vmeer {
 namespace pms {
 
-    // Implementasi filter_package_data (Yang dipanggil binder_engine.cpp)
-    bool filter_package_data(void* reply_parcel) {
-        if (!reply_parcel) return false;
-        
-        __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "PMS: Filtering Transaction via Parcel...");
-        // Di sini nanti kita bedah Parcel-nya
-        return true; 
+/**
+ * Singleton Get():
+ * Mengembalikan instance statis dari PMSRuntime.
+ */
+PMSRuntime& PMSRuntime::Get() {
+    static PMSRuntime instance;
+    return instance;
+}
+
+void PMSRuntime::RegisterPackage(const std::string& pkg_name, uint32_t v_uid) {
+    auto pkg = std::make_shared<VirtualPackage>();
+    pkg->package_name = pkg_name;
+    pkg->virtual_uid = v_uid;
+    pkg->is_visible = true;
+
+    m_registry[pkg_name] = pkg;
+    m_uid_to_pkg[v_uid] = pkg_name;
+    
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Registered: %s (vUID: %u)", pkg_name.c_str(), v_uid);
+}
+
+std::shared_ptr<VirtualPackage> PMSRuntime::GetPackage(const std::string& pkg_name) {
+    auto it = m_registry.find(pkg_name);
+    if (it != m_registry.end()) {
+        return it->second;
     }
+    return nullptr;
+}
 
-    // Pindahan dari pms_runtime.cpp
-    void filter_package_list(std::vector<std::string>& packages) {
-        std::vector<std::string> blacklisted = {
-            "com.topjohnwu.magisk",
-            "com.vmeer.manager",
-            "org.meowcat.edxposed.manager"
-        };
+uint32_t PMSRuntime::GetVirtualUid(const std::string& pkg_name) {
+    auto pkg = GetPackage(pkg_name);
+    return pkg ? pkg->virtual_uid : 0;
+}
 
-        packages.erase(
-            std::remove_if(packages.begin(), packages.end(), [&](const std::string& pkg) {
-                for (const auto& b : blacklisted) {
-                    if (pkg.find(b) != std::string::npos) {
-                        return true;
-                    }
+std::string PMSRuntime::GetPackageFromUid(uint32_t v_uid) {
+    auto it = m_uid_to_pkg.find(v_uid);
+    return (it != m_uid_to_pkg.end()) ? it->second : "";
+}
+
+bool PMSRuntime::IsVisible(const std::string& target_pkg) {
+    // Default visibility logic
+    if (target_pkg.find("com.vmeer") != std::string::npos) return false;
+    return true;
+}
+
+std::string PMSRuntime::ResolveVirtualPath(const std::string& pkg_name) {
+    return "/data/data/com.vmeer.manager/virtual/storage/" + pkg_name;
+}
+
+/**
+ * Bridge function untuk binder_engine.cpp
+ */
+bool filter_package_data(void* reply_parcel) {
+    if (!reply_parcel) return false;
+    // TODO: Implementasi Parcel stripping menggunakan libbinder internal
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "PMS: Intercepted Package Data Reply.");
+    return true;
+}
+
+void filter_package_list(std::vector<std::string>& packages) {
+    std::vector<std::string> blacklisted = {
+        "com.topjohnwu.magisk",
+        "com.vmeer.manager",
+        "org.meowcat.edxposed.manager"
+    };
+
+    packages.erase(
+        std::remove_if(packages.begin(), packages.end(), [&](const std::string& pkg) {
+            for (const auto& b : blacklisted) {
+                if (pkg.find(b) != std::string::npos) {
+                    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Hiding: %s", pkg.c_str());
+                    return true;
                 }
-                return false;
-            }), 
-            packages.end()
-        );
-    }
-
-    std::string get_virtual_installer(const std::string& target_pkg) {
-        (void)target_pkg;
-        return "com.android.vending";
-    }
+            }
+            return false;
+        }), 
+        packages.end()
+    );
+}
 
 } // namespace pms
 } // namespace vmeer
