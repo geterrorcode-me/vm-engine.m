d6d90ae82a5d83950dacf633c7a4c3a0fbafc533
getError <geterrorcode@gmail.com>
2026-05-15 19:45:36 +0700

    Commit

--- vmeer-module/src/main/jni/vmeer_fuse_ops.cpp
+++ vmeer-module/src/main/jni/vmeer_fuse_ops.cpp
@@ -1,32 +1,15 @@
+#include <jni.h>
 #include <fuse.h>
-#include <string.h>
-#include <errno.h>
 #include <android/log.h>
 
-#define LOG_TAG "vMeer_FUSE"
-
-// PENTING: Deklarasikan fungsi dari vmeer_squashfs_bridge.cpp 
-// agar compiler tahu fungsi ini ada di file lain.
+// Pastikan deklarasi dan definisi menggunakan extern "C"
 extern "C" {
-    int squash_get_metadata(const char *path, struct stat *stbuf);
-    int squash_list_dir(const char *path, void *buf, fuse_fill_dir_t filler);
+
+void vmeer_fuse_init(const char* rom, const char* mnt) {
+    __android_log_print(ANDROID_LOG_INFO, "vMeer_FUSE", "Mounting %s to %s", rom, mnt);
+    
+    // Logika mount FUSE kamu di sini...
+    // fuse_main(argc, argv, &vmeer_oper, NULL);
 }
 
-static int vmeer_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
-    (void)fi;
-    // Sekarang compiler mengenali fungsi ini
-    return squash_get_metadata(path, stbuf);
-}
-
-static int vmeer_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi,
-                         enum fuse_readdir_flags flags) {
-    (void)offset; (void)fi; (void)flags;
-    return squash_list_dir(path, buf, filler);
-}
-
-// Tambahkan definisi operasi FUSE lainnya jika diperlukan
-static const struct fuse_operations vmeer_oper = {
-    .getattr = vmeer_getattr,
-    .readdir = vmeer_readdir,
-};
+} // extern "C"
