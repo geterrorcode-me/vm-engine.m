884597479ed77ad1028c462ff34f0aece61dc3ab
getError <geterrorcode@gmail.com>
2026-05-19 23:40:36 +0700

    Commit

--- vmeer-module/src/main/jni/art_vm.cpp
+++ vmeer-module/src/main/jni/art_vm.cpp
@@ -87,7 +87,7 @@ bool InjectMirrorFramework(JNIEnv* env, jobject class_loader, const std::string&
 
     // Ambil nama file bersih untuk mempermudah pelacakan debugging logcat
     std::string clean_jar_name = jar_path.substr(jar_path.find_last_of("/\\") + 1);
-    [span_2](start_span)[span_3](start_span)[span_4](start_span)[span_5](start_span)[span_6](start_span)[span_7](start_span)[span_8](start_span)[span_9](start_span)[span_10](start_span)[span_11](start_span)LOGI("vMeer ART: Menjadwalkan penggabungan classpath -> %s", clean_jar_name.c_str());[span_2](end_span)[span_3](end_span)[span_4](end_span)[span_5](end_span)[span_6](end_span)[span_7](end_span)[span_8](end_span)[span_9](end_span)[span_10](end_span)[span_11](end_span)
+    LOGI("vMeer ART: Menjadwalkan penggabungan classpath -> %s", clean_jar_name.c_str());
 
     if (class_loader == nullptr) {
         LOGE("vMeer ART: Operasi dibatalkan, ClassLoader host bernilai NULL!");
@@ -110,7 +110,7 @@ bool InjectMirrorFramework(JNIEnv* env, jobject class_loader, const std::string&
         return false; 
     }
 
-    [span_12](start_span)jfieldID path_list_fid = env->GetFieldID(loader_clazz, "pathList", "Ldalvik/system/DexPathList;");[span_12](end_span)
+    jfieldID path_list_fid = env->GetFieldID(loader_clazz, "pathList", "Ldalvik/system/DexPathList;");
     if (!path_list_fid) { 
         LOGE("vMeer ART: Field 'pathList' tidak ditemukan pada ClassLoader."); 
         return false; 
@@ -125,7 +125,7 @@ bool InjectMirrorFramework(JNIEnv* env, jobject class_loader, const std::string&
     jclass path_list_clazz = env->GetObjectClass(path_list_obj);
 
     // 2. Ambil struktur array dexElements lama yang sedang berjalan pada aplikasi host
-    [span_13](start_span)jfieldID elements_fid = env->GetFieldID(path_list_clazz, "dexElements", "[Ldalvik/system/DexPathList$Element;");[span_13](end_span)
+    jfieldID elements_fid = env->GetFieldID(path_list_clazz, "dexElements", "[Ldalvik/system/DexPathList$Element;");
     if (!elements_fid) { 
         LOGE("vMeer ART: Field 'dexElements' sistem operasi tidak ditemukan."); 
         return false; 
@@ -216,7 +216,7 @@ bool InjectMirrorFramework(JNIEnv* env, jobject class_loader, const std::string&
 
     // 7. Kunci kembali struktur array baru ke dalam ClassLoader host aktif
     env->SetObjectField(path_list_obj, elements_fid, combined_elements);
-    [span_14](start_span)[span_15](start_span)[span_16](start_span)[span_17](start_span)[span_18](start_span)[span_19](start_span)[span_20](start_span)[span_21](start_span)[span_22](start_span)[span_23](start_span)LOGI("vMeer ART: INTEGRATION LIVE -> Komponen %s sukses disatukan ke runtime.", clean_jar_name.c_str());[span_14](end_span)[span_15](end_span)[span_16](end_span)[span_17](end_span)[span_18](end_span)[span_19](end_span)[span_20](end_span)[span_21](end_span)[span_22](end_span)[span_23](end_span)
+    LOGI("vMeer ART: INTEGRATION LIVE -> Komponen %s sukses disatukan ke runtime.", clean_jar_name.c_str());
 
     // Bersihkan seluruh referensi memori lokal JNI lokal untuk mencegah kebocoran tabel referensi
     env->DeleteLocalRef(j_path);
